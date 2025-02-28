1b07a227ec4f08441d5ee1abe823020bcf8d58c1
Daniz.Dev <jose.daniel.alicrim9@hotmail.com>
2025-02-27 21:05:50 -0300

    Revisao de Java, entendendo alguns comandos.

---
+++ RevisaoJava/src/etecuirapuru/com/br/main/Main.java
@@ -0,0 +1,43 @@
+package etecuirapuru.com.br.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner lerOQueUsuarioDigita = new Scanner(System.in);
+        String nomeUsuario;
+
+        System.out.println("Digite seu nome: ");
+        nomeUsuario = lerOQueUsuarioDigita.next();
+
+        int rmAluno;
+
+        System.out.println("Digite o seu RM: ");
+        rmAluno = lerOQueUsuarioDigita.nextInt();
+
+        String isQuerIrEmboraCedo;
+        System.out.println("Quer ir embora mais cedo? (S / N)");
+        isQuerIrEmboraCedo = lerOQueUsuarioDigita.next();
+           
+        if(isQuerIrEmboraCedo.toLowerCase().equals("s")){
+            isQuerIrEmboraCedo = "Sim";
+        }else{
+            isQuerIrEmboraCedo = "Não";
+        }
+        
+        System.out.println("");
+        System.out.println("-----INFORMAÇÕES ALUNO -------");
+        System.out.println("");
+        System.out.println("Nome do usuario: " + nomeUsuario
+                + "\nRm do aluno: " + rmAluno +
+                "\nPretende sair cedo: " + isQuerIrEmboraCedo
+        );
+
+        System.out.println("");
+        System.out.println("----- ------- -------");
+        System.out.println("");
+
+    }
+}
